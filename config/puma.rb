081682ac5dee11aa8c5003ca90ab2e5356f5b6fd
Pablo Garcia <pablodgcatulo@gmail.com>
2015-05-27 23:49:19 -0300

    modificados parametros para puma en produccion

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,15 @@
-workers ENV.fetch('WEB_CONCURRENCY') { 2 }.to_i
-
-threads_count = ENV.fetch('MAX_THREADS') { 5 }.to_i
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-rackup DefaultRackup
-port ENV.fetch('PORT') { 3000 }
-environment ENV.fetch('RACK_ENV') { 'development' }
 preload_app!
 
-on_worker_boot do
-  # worker specific setup
-  ActiveRecord::Base.establish_connection
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
-  # If you are using Redis but not Resque, change this
-  if defined?(Resque)
-    Resque.redis = ENV["OPENREDIS_URL"] || "redis://127.0.0.1:6379"
-    Rails.logger.info('Connected to Redis')
-  end
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
